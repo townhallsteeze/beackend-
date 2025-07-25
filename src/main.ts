7d912510d9d25182c48a8bad493024a94ed5f395
nylehia <nyshabazz@icloud.com>
2025-07-25 07:28:50 -0400

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors({
+    origin: 'https://mrbeast-lyart.vercel.app', // your frontend URL
+    credentials: true, // set to true if you're using cookies or auth headers
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  })
   await app.listen(process.env.PORT ??  4000);
 }
 bootstrap();
